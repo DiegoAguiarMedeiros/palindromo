edc18d572b72b823ef5c8c81275e3347a879fde3
Diego Aguiar Medeiros <diego.a.medeiros@gmail.com>
2023-08-02 20:57:17 -0300

    should return true if 'Amor a Roma' is passed as parameter

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 const isPalindromo = (text: string): boolean => {
   if (text === "") return false;
   let reverseText = text.split("");
-  return text.toLowerCase() === reverseText.join("").toLowerCase();
+  return (
+    text.toLowerCase().trim() === reverseText.join("").toLowerCase().trim()
+  );
 };
 
 export default isPalindromo;
