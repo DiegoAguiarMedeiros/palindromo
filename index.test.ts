2f8d4cb25ce266023d7d67b61565ef71b65590e8
Diego Aguiar Medeiros <diego.a.medeiros@gmail.com>
2023-08-02 20:56:02 -0300

    should return true if 'Amor a Roma' is passed as parameter

--- index.test.ts
+++ index.test.ts
@@ -11,3 +11,7 @@ test("should accept a string as parameter", () => {
 test("should return false if '' is passed as parameter", () => {
   expect(isPalindromo("")).toBe(false);
 });
+
+test("should return true if 'Amor a Roma' is passed as parameter", () => {
+  expect(isPalindromo("Amor a Roma")).toBe(true);
+});
--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 const isPalindromo = (text: string): boolean => {
   if (text === "") return false;
+  let reverseText = text.split("");
+  return text.toLowerCase() === reverseText.join("").toLowerCase();
 };
 
 export default isPalindromo;
