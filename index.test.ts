28f76e5959a7d4a3014492cb489f416a2915e680
Diego Aguiar Medeiros <diego.a.medeiros@gmail.com>
2023-08-02 21:15:58 -0300

    removed duplicate test

--- index.test.ts
+++ index.test.ts
@@ -1,9 +1,5 @@
 import isPalindromo from "./index";
 
-test("should import the function", () => {
-  isPalindromo("");
-});
-
 test("should accept a string as parameter", () => {
   isPalindromo("");
 });
